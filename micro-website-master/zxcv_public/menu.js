f33452669b9e1d95fd0ef916d43ad78f469a9946
ershu <273510509@qq.com>
2019-12-10 16:24:15 +0800

    所有界面的iframe

--- micro-website-master/zxcv_public/menu.js
+++ micro-website-master/zxcv_public/menu.js
@@ -2,20 +2,16 @@ function clickMenu(target) {
     switch(target){
         case 'index':
             // 加载页面
-            window.open('../zxcv_index/index.html');
-            // $('.content').load('../zxcv_home/home.html');
+            window.open("../zxcv_index/index.html", "_top");
             break;
         case 'news':
-            window.open('../zxcv_news/news.html');
-            // $('.content').load('../zxcv_news/news.html');
+            window.open("../zxcv_news/news.html", "_top");
             break;
-        case 'info':
-            window.open('../zxcv_info/info.html');
-            // $('.content').load('../zxcv_info/info_content.html');
+        case 'product':
+            window.open("../zxcv_product/product.html", "_top");
             break;
         case 'user':
-            window.open('../zxcv_user/user.html');
-            // $('.content').load('../zxcv_user/user.html');
+            window.open("../zxcv_user/user.html", "_top");
             break;
         default:
             break;
