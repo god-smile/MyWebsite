2f4a280fc1a94fd5f46c8c1636081a17f7ee1047
ershu <273510509@qq.com>
2020-03-21 12:25:21 +0800

    error 页面

--- website/js/common/ajax.js
+++ website/js/common/ajax.js
@@ -164,7 +164,7 @@ function getAjax(opts) {
         success: function () {
         },
         error: function () {
-            ErrorAlertManual("系统出错，请重试或联系管理员！");
+            errorPage();
         }
     };
     //二.用户参数覆盖默认参数
@@ -188,96 +188,9 @@ function getAjax(opts) {
         },
         success: function (res, status, xhr) {
 
-            if (res.code == "2003") {
-                //授权令牌不存在或已失效，请重新登录后在尝试
-                /*ErrorAlertManual("登录信息失效，请重新登录！");
-                parent.open("../zxcv_login/login.html", "_top");*/
-
-                AlertAndOpen("登录信息失效，请重新登录！", "../zxcv_login/login.html");
-            } else {
-                defaults.success(res, status, xhr);
-            }
-
         },
         error: function (XMLHttpRequest, textStatus, errorThrown) {
-            /*if (textStatus == "timeout") {
-                //alert('请求超时，请重试');
-            } else {
-                //alert("请求报错")
-                console.log(errorThrown);
-            }*/
-            defaults.error();
+            errorPage();
         }
     });
 }
-
-/**
- * 登录ajax
- * @param opts
- */
-function loginAjax(opts) {
-
-    //一.设置默认参数
-    var defaults = {
-        method: 'POST',
-        url: '',
-        dataType: 'json',
-        data: '',
-        async: true,
-        cache: false,
-        processData: true,
-        contentType: 'application/json; charset=utf-8',
-        success: function () {
-        },
-        error: function () {
-            ErrorAlertManual("系统出错，请重试或联系管理员！");
-        }
-    };
-    //二.用户参数覆盖默认参数
-    for (var key in opts) {
-        defaults[key] = opts[key];
-    }
-    $.ajax({
-        type: defaults.method,
-        url: defaults.url,
-        dataType: defaults.dataType,
-        contentType: defaults.contentType,
-        data: defaults.data,
-        async: defaults.async,
-        processData: defaults.processData,
-        beforeSend: function (xhr) {
-            //设置请求头
-            //xhr.setRequestHeader("User-Agent", "headertest");
-            //console.log(JSON.stringify(sysComm));
-            //xhr.setRequestHeader("x-auth-token","43399b23-b673-4f1e-97d6-5ee6105a860c");
-
-        },
-        success: function (res, status, xhr) {
-            defaults.success(res, status, xhr);
-
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-            /*if (textStatus == "timeout") {
-                //alert('请求超时，请重试');
-            } else {
-                //alert("请求报错")
-                console.log(errorThrown);
-            }*/
-            defaults.error();
-        }
-    });
-}
-
-var documentBindFunc = {
-    /**
-     * @param event  事件名称如：click
-     * @param element 元素 如：id,class 元素等
-     * @param func 函数
-     * 例子：documentBindFunc.on('click','#berthmanage-addBtn',function () {});
-     */
-    on: function (event, element, func) {
-        $(document).off(event, element);
-        $(document).on(event, element, func);
-    }
-
-};
